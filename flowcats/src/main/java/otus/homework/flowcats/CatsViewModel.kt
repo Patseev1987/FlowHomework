39851150782ec2a1b9a969508323f3ebfd80aa5c
antonkazakov <antonanton08@gmail.com>
2024-05-08 22:32:40 +0400

    updated infrastructure

--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -1,7 +1,11 @@
 package otus.homework.flowcats
 
 import androidx.lifecycle.*
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -10,14 +14,16 @@ class CatsViewModel(
     private val catsRepository: CatsRepository
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Fact>()
-    val catsLiveData: LiveData<Fact> = _catsLiveData
-
+    private val _catsStateFlow: MutableStateFlow<Result> = MutableStateFlow(Success(Fact.NONE))
+    val catsStateFlow: StateFlow<Result> = _catsStateFlow.asStateFlow()
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        _catsStateFlow.value = Error(throwable.message ?: throwable.toString())
+    }
     init {
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
+        viewModelScope.launch ( exceptionHandler){
+            withContext(Dispatchers.IO ) {
                 catsRepository.listenForCatFacts().collect {
-                    _catsLiveData.value = it
+                    _catsStateFlow.value = Success(it)
                 }
             }
         }
@@ -26,6 +32,10 @@ class CatsViewModel(
 
 class CatsViewModelFactory(private val catsRepository: CatsRepository) :
     ViewModelProvider.NewInstanceFactory() {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
         CatsViewModel(catsRepository) as T
 }
+
+sealed class Result
+data class Success(val fact: Fact) : Result()
+data class Error(val message: String) : Result()
--- flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
+++ flowcats/src/main/java/otus/homework/flowcats/MainActivity.kt
@@ -1,8 +1,16 @@
 package otus.homework.flowcats
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +22,25 @@ class MainActivity : AppCompatActivity() {
         val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
         setContentView(view)
 
-        catsViewModel.catsLiveData.observe(this){
-            view.populate(it)
+        observeFlow(this, catsViewModel.catsStateFlow, view)
+    }
+
+    private fun observeFlow(lifecycleOwner: LifecycleOwner, state: StateFlow<Result>, view: CatsView) {
+        lifecycleOwner.lifecycleScope.launch {
+            lifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                state.collect { state ->
+                    when (state) {
+                        is Error -> Toast.makeText(
+                            this@MainActivity,
+                            state.message, Toast.LENGTH_LONG
+                        ).show()
+
+                        is Success -> {
+                            view.populate(state.fact)
+                        }
+                    }
+                }
+            }
         }
     }
 }
